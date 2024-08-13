7d30cd6c26f7560e1335721ee11a23de4fadf5ef
Than Htike Zaw <thanhtikezaw@onenex.co>
2024-08-14 00:13:02 +0700

    add note for nodemon usage

--- Node Day 1/simple-webserver.js
+++ Node Day 1/simple-webserver.js
@@ -1,9 +1,11 @@
 const http = require("http");
 
-const server = http.createServer(() => {
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
 
-})
+//can use nodemon to restart server for every change and without stopping the node
 
 server.listen(3000, function () {
   console.log("Successfully Started");
-})
+});
